831a826090f79b28994be7ba16433073d48e9bd1
juancarlosf2 <jceldominicano@gmail.com>
2020-09-01 08:58:18 -0400

    added router-dom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// changes
 function App() {
   return (
     <div className="App">
