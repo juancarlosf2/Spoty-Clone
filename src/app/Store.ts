0b7d868bffeb1afd1b0d7176afe6c0858e3b8e1c
juancarlosf2 <jceldominicano@gmail.com>
2021-03-14 12:50:50 -0400

    add useThunkDispatch and useTypedSelector

--- src/app/Store.ts
+++ src/app/Store.ts
@@ -1,4 +1,5 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import rootReducer, { RootState } from "./RootReducer";
 
 const store = configureStore({ reducer: rootReducer });
@@ -7,4 +8,6 @@ export type AppDispatch = typeof store.dispatch;
 
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
+export const useThunkDispatch = () => useDispatch<AppDispatch>();
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 export default store;
